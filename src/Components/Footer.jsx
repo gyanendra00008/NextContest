ede8ca21fb5ad56886d254074e65380872e63747
Gyanendra <gk154866@gmail.com>
2026-07-08 07:45:56 +0530

    improved Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,18 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div id='main-footer'>
-        <h2>
-            Made by Gyanendra Kumar ,, NIT allahabad CSE
-        </h2>
+<h2>Stay updated with coding contests from multiple platforms in one place.</h2>
+
+             <div id='contacts'>
+             <a href="https://github.com/gyanendra00008/NextContest"> GitHub</a> 
+              <h2>•</h2>
+              <a href="https://www.linkedin.com/in/gyanendra-kumar-2138b23a2/">LinkedIn </a>
+              <h2>•</h2>
+               <a href="mailto:gk154866@gmail.com">Contact</a>              
+              </div>
+
+<h2>© 2026 NextContest. All rights reserved.</h2>
+<h2>Made with ❤️ by Gyanendra Kumar</h2>
     </div>
   )
 }
